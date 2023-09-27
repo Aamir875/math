48180efef4a72eb4578d317fea8f3a9192b4ff18
Aamir <aamirkhan87589@gmail.com>
2023-09-27 13:18:55 +0530

    modified comment in fucntion body

--- mathematical_operations/src/multiply.cpp
+++ mathematical_operations/src/multiply.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 void multiply(int a, int b){
-//Below line will print the a*b.
+//Below line will print the a*b i.e, multiplication of two numbers.
 std::cout<<"multiply is "<<a*b;
 }
